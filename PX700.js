841a9660b4373ea46e6f3f8e20510791d2ac2779
Isaac Waller <isaac@siriusapplications.com>
2016-03-22 19:56:48 -0700

    Initial commit

---
+++ PX700.js
@@ -0,0 +1,361 @@
+'use strict';
+
+var SerialPort = require("serialport").SerialPort;
+const EventEmitter = require('events');
+
+// Indexed by device type
+const CondiCommands = {
+  0x01: { // 'keypad'
+    "power-buttom-down": 0x10,
+    "power-button-up": 0x11,
+    "power-button-long": 0x12,
+    "power-led-on": 0x32,
+    "power-led-off": 0x33,
+    "fm-and-power-led-on": 0x30,
+    "all-leds-off": 0x31,
+    "fm-button-down": 0x01,
+    "fm-button-up": 0x02,
+    "fm-button-long": 0x03,
+    "fm-led-on": 0x21,
+    "fm-led-off": 0x22,
+    "cd-button-down": 0x04,
+    "cd-button-up": 0x05,
+    "cd-button-long": 0x06,
+    "cd-led-on": 0x24,
+    "cd-led-off": 0x25,
+    "tape-button-down": 0x07,
+    "tape-button-up": 0x08,
+    "tape-button-long": 0x09,
+    "tape-led-on": 0x27,
+    "tape-led-off": 0x28,
+    "aux-button-down": 0x0A,
+    "aux-button-up": 0x0B,
+    "aux-button-long": 0x0C,
+    "aux-led-on": 0x2A,
+    "aux-led-off": 0x2B,
+    "vid-button-down": 0x0D,
+    "vid-button-up": 0x0E,
+    "vid-button-long": 0x0F,
+    "vid-led-on": 0x2D,
+    "vid-led-off": 0x2E,
+    "volume-up": 0x71,
+    "volume-down": 0xB1,
+    "mute": 0xF1,
+
+    "acknowledge": 0x38
+  },
+  0x04: { // 'computer'
+   "room-on": 0x40,
+   "room-off": 0x41,
+   "room-all-on": 0x42,
+   "room-all-off": 0x43,
+   "mute-on": 0xF2,
+   "mute-off": 0xF3,
+   "select-tuner": 0x01,
+   "select-next-tuner": 0x03,
+   "select-cd": 0x04,
+   "select-next-cd": 0x06,
+   "select-tape": 0x07,
+   "select-next-tape": 0x09,
+   "select-aux": 0x0A,
+   "select-next-aux": 0x0C,
+   "select-video": 0x0D,
+   "select-next-video": 0x0F,
+   "select-page": 0x44,
+   "main-room-status-request": 0x50,
+   "main-room-status-response": 0x63,
+   "set-main-room-level": 0x71,
+
+   "acknowledge": 0x38
+  }
+};
+const ComputerCommands = CondiCommands[0x04];
+
+const CondiCommandNames = function (obj) {
+  var new_obj = {};
+  for (var device in obj) {
+    if(obj.hasOwnProperty(device)) {
+      var new_device = {};
+      for (var byte in obj[device]) {
+        if(obj[device].hasOwnProperty(byte)) {
+          new_device[obj[device][byte]] = byte;
+        }
+      }
+      new_obj[device] = new_device;
+    }
+  }
+  return new_obj;
+}(CondiCommands);
+
+const CondiCommandDataLengths = {
+  0x01: { // 'keypad'
+
+  },
+  0x04: { // 'computer'
+    0x63: 4,
+    0x71: 3,
+    0x62: 3,
+    0x70: 3
+  }
+};
+
+const MESSAGE_SPACING = 100;
+const ACK_TIMEOUT = 5000;
+
+class PX700 extends EventEmitter {
+  // Create new connection, do not use until you have received the 'ready'
+  // event
+  constructor(path) {
+    super();
+    var serialPort = new SerialPort(path, {
+      baudrate: 9600,
+      dataBits: 8,
+      stopBits: 1,
+      parity: 'none'
+    });
+
+    var self = this;
+    serialPort.on("open", function () {
+      serialPort.on('data', function(data) {
+        self.parseIncomingData(data);
+      });
+
+      self.emit('ready');
+    });
+
+    this.serialPort = serialPort;
+  }
+
+  parseIncomingData(buffer) {
+
+    if (buffer[0] != 0x7E) {
+      console.log("Recieved bad preamble, skipping message");
+      return;
+    }
+
+    var systemCode = buffer[1]; // Ignored for now
+    var zoneCode = buffer[2]; // Ignored for now
+
+    var deviceTypeByte = buffer[3], deviceType;
+    switch (deviceTypeByte) {
+    case 0x01:
+      deviceType = 'keypad';
+      break;
+    case 0x04:
+      deviceType = 'computer';
+      break;
+    case 0x06:
+      deviceType = 'px-603';
+      break;
+    case 0x05:
+    case 0x07:
+      deviceType = 'reserved';
+      break;
+    default:
+      deviceType = 'unknown (' + deviceTypeByte.toString(16) + ')';
+      break;
+    }
+
+    var command = buffer[4];
+
+    var data;
+    var dataLength = 0;
+    if (CondiCommandDataLengths[deviceTypeByte][command]) {
+      dataLength = CondiCommandDataLengths[deviceTypeByte][command];
+      data = buffer.slice(5, 5 + dataLength);
+    }
+
+    var checksum = buffer[5 + dataLength];
+    // TODO verify checksum
+
+    var commandName = CondiCommandNames[deviceTypeByte][command];
+    if (!commandName) {
+      commandName = 'unknown (' + command.toString(16) + ')';
+    }
+
+    var msg = {
+      deviceType: deviceType,
+      command: commandName
+    };
+
+    if (dataLength > 0) {
+      msg.data = data;
+    }
+
+    this.emit('message', msg);
+    // Used for not sending messages while others are using the connection
+    this.lastMessageReceiveTime = new Date();
+
+    if (buffer.length > (6 + dataLength)) {
+      // additional commands
+      this.parseIncomingData(buffer.slice(6 + dataLength));
+    }
+  }
+
+  // Lets you use EventEmitter.once with a timeout
+  onceTimeout(event, timeout, callback) {
+    var timer;
+    var self = this;
+
+    var handler = function () {
+      clearTimeout(timer);
+      var args = Array.prototype.slice.call(arguments);
+      args.unshift(null); // 'err'
+      callback.apply(this, args);
+    };
+
+    timer = setTimeout(function () {
+      self.removeListener(event, handler);
+      callback(new Error("Timed out"));
+    }, timeout);
+
+    self.once(event, handler);
+  }
+
+  // Only use after open
+  sendString(hex, callback) {
+    // From the CONDI manual:
+    // Before beginning data transmission, the bus should be checked for inactivity.
+    // If the bus is free, transmission may commense. If the bus is being used, wait for a period of inactivity.
+    if ((new Date() - this.lastMessageReceiveTime) < MESSAGE_SPACING) {
+      // Wait
+      setTimeout(() => this.sendString(hex, callback), MESSAGE_SPACING);
+      return;
+    }
+    this.serialPort.write(new Buffer(hex.replace(/ /g,'').toLowerCase(), "hex"), function(err) {
+      callback(err);
+    });
+  }
+
+  sendCommand(zone, command, data, callback, skipAck) {
+    // First, create command string
+    var commandByte = ("0" + ComputerCommands[command].toString(16)).substr(-2);
+    var string = "7E 10 0" + zone + " 04 " + commandByte;
+    if (data) {
+      string += " ";
+      string += data;
+    }
+
+    console.log("Sending command " + string);
+    var checksum = -(string.split(' ').map(function (s) { return parseInt(s, 16); }).reduce((a, b) => a + b, 0) + 1) & 0xFF;
+    var self = this;
+
+    // Send command, then wait for echo and ACK
+    this.sendString(string + checksum.toString(16), function (err) {
+      // Should get echo first
+      self.onceTimeout('message', ACK_TIMEOUT, function (err, msg) {
+        if (err) {
+          callback(err);
+          return;
+        }
+
+        if (msg.command == command) {
+          console.log("Got echo");
+          // Good echo
+          if (skipAck) {
+            callback();
+          } else {
+            // Wait for ACK
+            self.onceTimeout('message', ACK_TIMEOUT, function (err, msg) {
+              if (err) {
+                callback(err);
+                return;
+              }
+
+              if (msg.command == 'acknowledge') {
+                callback();
+              } else {
+                callback(new Error("Bad ACK", msg));
+              }
+            });
+          }
+        } else {
+          callback(new Error("Bad echo", msg));
+        }
+      });
+    });
+  }
+
+  muteOn(zone, callback) {
+    this.sendCommand(zone, 'mute-on', null, callback);
+  }
+
+  muteOff(zone, callback) {
+    this.sendCommand(zone, 'mute-off', null, callback);
+  }
+
+  roomOn(zone, callback) {
+    this.sendCommand(zone, 'room-on', null, callback);
+  }
+
+  roomOff(zone, callback) {
+    this.sendCommand(zone, 'room-off', null, callback);
+  }
+
+  // Set volume on a specific zone
+  // level should be from 0 to 63
+  // Unity gain is 53
+  setVolume(zone, level, callback) {
+    var levelByte = ("0" + level.toString(16)).substr(-2);
+    this.sendCommand(zone, 'set-main-room-level', levelByte + " 07 07", function (err, results) {
+      callback(err);
+    });
+  }
+
+  selectTuner(zone, callback) {
+    this.sendCommand(zone, 'select-tuner', null, callback);
+  }
+
+  getMainRoomStatus(zone, callback) {
+    var self = this;
+    this.sendCommand(zone, 'main-room-status-request', null, function (err) {
+      if (err) {
+        callback(err);
+      } else {
+        self.onceTimeout('message', ACK_TIMEOUT, function (err, msg) {
+          if (err) {
+            callback(err);
+            return;
+          }
+
+          if (msg.command != 'main-room-status-response') {
+            callback(new Error("received bad response"));
+          } else {
+            // Parse main room status response.
+            var sourceByte = msg.data[0];
+            var source;
+            switch (sourceByte) {
+            case 0x00:
+              source = "off";
+              break;
+            case 0x01:
+              source = "tuner";
+              break;
+            case 0x02:
+              source = "cd";
+              break;
+            case 0x03:
+              source = "tape";
+              break;
+            case 0x04:
+              source = "aux";
+              break;
+            case 0x05:
+              source = "video";
+              break;
+            }
+
+            callback(null, {
+              source: source,
+              volume: msg.data[1],
+              bass: msg.data[2],
+              treble: msg.data[3]
+            });
+          }
+        });
+      }
+    }, true); // Do not wait for ACK
+  }
+}
+
+module.exports = PX700;
